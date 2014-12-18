1cf5a79864cc5b65f73e78a075356f3994b787b9
Brianna <brianna.chesser@gmail.com>
2014-12-18 13:46:10 -0800

    created class GoldenMasterTest to run code once & buffer output as $output variable; installed phpunit via homebrew

---
+++ trivia_tests/GoldenMasterTest.php
@@ -0,0 +1,12 @@
+<?php
+class GoldenMasterTest extends PHPUnit_Framework_TestCase {
+
+    function testGenerateOutput() {
+        ob_start();
+        require_once dirname(dirname(__FILE__)) . '/php/GameRunner.php'; //run code in this file
+        $output = ob_get_contents(); //buffers output into $output variable
+        ob_end_clean();
+
+        var_dump($output);
+    }
+}
