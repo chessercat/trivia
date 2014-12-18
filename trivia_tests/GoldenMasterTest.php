71d548358e9f6a99b68a24941113a08e01ae7d6c
Brianna <brianna.chesser@gmail.com>
2014-12-18 14:43:14 -0800

    add srand(1) to GoldenMasterTest so that output of GameRunner random number generator is controlled for testing purposes & the results are the same if it's not broken

--- trivia_tests/GoldenMasterTest.php
+++ trivia_tests/GoldenMasterTest.php
@@ -3,6 +3,7 @@ class GoldenMasterTest extends PHPUnit_Framework_TestCase {
 
     function testGenerateOutput() {
         ob_start();
+        srand(1); //seed random number generator with 1 so that output is always the same for testing purposes
         require_once dirname(dirname(__FILE__)) . '/php/GameRunner.php'; //run code in this file
         $output = ob_get_contents(); //buffers output into $output variable
         ob_end_clean();
